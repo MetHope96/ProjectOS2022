99825f1fa31a0060bc876da43dfbd36fc086f3b7
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2022-12-12 15:55:51 +0100

    sys_write DA

--- os161/os161-base-2.0.3/kern/syscall/file_syscall.c
+++ os161/os161-base-2.0.3/kern/syscall/file_syscall.c
@@ -22,32 +22,37 @@ int sys_write(int fd, userptr_t buff, size_t buff_len, int *retval){
     return EBADF; //Fd is not a valid file descriptor
   }
 
-  
+/*  
   char *buffer = (char *)kmalloc(sizeof(*buff)*buff_len);//buff user, buffer kernel
   err = copyin((const_userptr_t)buff, buffer, buff_len);
   if(err) {
     kfree(buffer);
     return err;
   }
-
+*/
 
   struct iovec iov;
   struct uio kuio;
 
   lock_acquire(curproc->file_table[fd]->lock);
-  uio_kinit(&iov, &kuio, buffer, buff_len, curproc->file_table[fd]->offset, UIO_WRITE);
-  //uio_kinit(&iov, &kuio, buff, buff_len, curproc->file_table[fd]->offset, UIO_WRITE);
+
+//  uio_kinit(&iov, &kuio, buffer, buff_len, curproc->file_table[fd]->offset, UIO_WRITE);
+    uio_uinit(&iov, &kuio, buff, buff_len, curproc->file_table[fd]->offset, UIO_WRITE);
+    u.uio_space = curproc->p_addrspace;
+	u.uio_segflg = UIO_USERSPACE; // for user space address
 
   err = VOP_WRITE (curproc->file_table[fd]->vnode, &kuio);
   if (err){
-    kfree(buffer);
+  //  kfree(buffer);
     return err;
   }
 
   *retval = buff_len - kuio.uio_resid; //calculate the amount of bytes written. 0 in case of success.
 
+
   curproc->file_table[fd]->offset = kuio.uio_offset;
+
   lock_release(curproc->file_table[fd]->lock);
-  kfree(buffer);
+//  kfree(buffer);
   return 0;
 }
