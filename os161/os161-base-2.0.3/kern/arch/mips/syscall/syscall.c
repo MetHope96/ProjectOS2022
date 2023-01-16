1b41501a7c6d5b184e13f63a1f283cf2959eb786
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2023-01-16 10:17:22 +0100

    waitpid & execv

--- os161/os161-base-2.0.3/kern/arch/mips/syscall/syscall.c
+++ os161/os161-base-2.0.3/kern/arch/mips/syscall/syscall.c
@@ -173,14 +173,14 @@ syscall(struct trapframe *tf)
 		err = 0;
 		break;
 
+		case SYS_waitpid:
+		err = sys_waitpid((pid_t)tf->tf_a0, (int *)tf->tf_a1, (int)tf->tf_a2,(pid_t *)&retval);
+		break;
 
-		//case SYS_waitpid:
-		//err = sys_waitpid((pid_t)tf->tf_a0, (int *)tf->tf_a1, (int)tf->tf_a2, &retval );
-		//break;
-/*
 		case SYS_execv:
-		err = SYS__execv((char *)tf->tf_a0, (char **)tf->tf_a1);
-		*/
+		err = sys_execv((const char *)tf->tf_a0,  (char **)tf->tf_a1);
+		break;
+
 	    default:
 		kprintf("Unknown syscall %d\n", callno);
 		err = ENOSYS;
