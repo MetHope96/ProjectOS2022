31ff88332e6e1346615c51742ae59983b4422725
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2023-01-23 17:30:50 +0100

    emu e trap

--- os161/os161-base-2.0.3/kern/arch/mips/locore/trap.c
+++ os161/os161-base-2.0.3/kern/arch/mips/locore/trap.c
@@ -39,6 +39,9 @@
 #include <vm.h>
 #include <mainbus.h>
 #include <syscall.h>
+#include <proc_syscall.h>
+#include <kern/wait.h>
+#include <proc.h>
 
 
 /* in exception-*.S */
@@ -111,7 +114,17 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 	/*
 	 * You will probably want to change this.
 	 */
+	curproc->exit_status = 1;
+	if(sig == SIGSEGV){
+		curproc->exit_code = _MKWAIT_CORE(sig);
+	}
+	else{
+		curproc->exit_code = _MKWAIT_SIG(sig);
+	}
 
+	thread_exit();
+
+	
 	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
 		code, sig, trapcodenames[code], epc, vaddr);
 	panic("I don't know how to handle this\n");
