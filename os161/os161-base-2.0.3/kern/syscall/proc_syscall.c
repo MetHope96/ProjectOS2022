c782b66f5cec939fd99c63eadf4fd7df5a734af6
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2023-01-25 11:50:28 +0100

    fix

--- os161/os161-base-2.0.3/kern/syscall/proc_syscall.c
+++ os161/os161-base-2.0.3/kern/syscall/proc_syscall.c
@@ -115,13 +115,12 @@ void sys_exit(int exitcode){
 		if(i == MAX_PROC - 1)
 		panic("Current process not found in process table");
 	}
-	curproc->exit_status = true;
 
-	lock_acquire(curproc->lock);
+	curproc->exit_status = true;
 	curproc->exit_code = _MKWAIT_EXIT(exitcode);
 	KASSERT(curproc->exit_status == proc_table[i]->exit_status);
 	KASSERT(curproc->exit_code == proc_table[i]->exit_code);
-	lock_release(curproc->lock);
+
 	thread_exit();
 }
 
