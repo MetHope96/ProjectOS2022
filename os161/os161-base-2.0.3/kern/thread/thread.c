9ab8a48fb7a1fcc79d425d017f789fdf49c45e0a
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2023-01-25 15:55:53 +0100

    Update thread.c

--- os161/os161-base-2.0.3/kern/thread/thread.c
+++ os161/os161-base-2.0.3/kern/thread/thread.c
@@ -271,6 +271,8 @@ thread_destroy(struct thread *thread)
 	 * If you add things to struct thread, be sure to clean them up
 	 * either here or in thread_exit(). (And not both...)
 	 */
+	if (cur->t_proc!=NULL)
+	  proc_remthread(cur);
 
 	/* Thread subsystem fields */
 	KASSERT(thread->t_proc == NULL);
