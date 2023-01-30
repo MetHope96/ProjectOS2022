1a7bd0a7136c7dc203b0bc5f9db90c509c51809c
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2023-01-30 11:43:16 +0100

    execv test

--- os161/test/execv_test.c
+++ os161/test/execv_test.c
@@ -11,31 +11,20 @@
 #include <string.h>
 #include <errno.h>
 
+#define SUBARGC_MAX 64
+static char *subargv[SUBARGC_MAX];
+
 int main(int argc, char **argv){
 	
-    pid_t pid;
-    int rval, status;
-    const char *prog = "testbin/helloworld";
+    static char default_prog[] = "/bin/pwd";
+    subargv[0] = default_prog;
 
-    printf("Execution fork\n");
-
-    pid = fork();
-
-    if(pid < 0){ // Error
-        printf("Error fork: %s\n",strerror(errno));
-        return 1;
-    }else if(pid == 0){ // Child
-        printf("Child process execution of execv\n");
+    int rval, status;;
         
-        rval = execv(prog, 0);
+        rval = execv(subargv[0], subargv);
         if (rval == -1){
             printf("Error execv: %s\n",strerror(errno));
             return rval;
         }
-    }else{ // Parent
-        waitpid(pid, &status, 0);
-        printf("This is printed the parent process. Status is %d, the child pid is %d.\n",status,pid);
-    }
-
 	return 0;
 }
