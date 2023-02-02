4211243b08b7dd027f95d1108c912fd28d75659f
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2023-02-02 15:47:26 +0100

    Revert "Revert "Update fork""
    This reverts commit baab417327797b887ecea87ba79aac430ec1b148.

--- os161/os161-base-2.0.3/kern/syscall/proc_syscall.c
+++ os161/os161-base-2.0.3/kern/syscall/proc_syscall.c
@@ -21,71 +21,61 @@ int sys_getpid(pid_t *curproc_pid) {
 	return 0;
 }
 
-static void op_efp(void *tfv, unsigned long data2){
-  (void) data2;
-  struct trapframe *tf = (struct trapframe *)tfv;
-  enter_forked_process(tf);
-}
-
 int sys_fork(pid_t *child_pid, struct trapframe *tf){
 
   struct trapframe *tf_child; //Stack of the exception handled
-  struct proc *child_proc = NULL;
+  struct proc *child_proc=NULL;
   int err;
 
-  if (proc_counter >= PID_MAX){
+  if (proc_counter >= MAX_PROC){
     return ENPROC; //There are too many process on the system.
   }
-  child_proc = proc_create_runprogram("child_proc");
+
+  child_proc = proc_create("name");
 
   if(child_proc == NULL){
     return ENOMEM; //Sufficient virtual memory for the new process was not available.
   }
 
-  /* Search the first free space in proc_table */
-
-    if(proc_counter == MAX_PROC -1){
-      return ENPROC; //There are already too many process on the system
-    }
-
-  /*Copy the address space of the parent in a child process */
-  err = as_copy(curproc->p_addrspace, &child_proc->p_addrspace);
-
-  if(err){
-    return err;
-  }
-
   child_proc->parent_id = curproc->proc_id;
 
-  /*Creation of memory space of trapframe */
-  tf_child = (struct trapframe *)kmalloc(sizeof(struct trapframe));
-  if(tf_child == NULL){
-    return ENOMEM; //Sufficient virtual memory for the new process was not available.
-  }
+    spinlock_acquire(&curproc->p_lock);
+      if (curproc->p_cwd != NULL) {
+            VOP_INCREF(curproc->p_cwd);
+            child_proc->p_cwd = curproc->p_cwd;
+      }
+    spinlock_release(&curproc->p_lock);
 
-  /* Copy the trapframe into trapframe_child */
-  *tf_child = *tf;
-  //memcpy(tf_child, tf, sizeof(struct trapframe));
+    as_copy(proc_getas(), &(child_proc->p_addrspace));
+    if(child_proc->p_addrspace == NULL){
+    proc_destroy(child_proc); 
+    return ENOMEM; 
+    }
+
+	tf_child = kmalloc(sizeof(struct trapframe));
+	if(tf_child == NULL){
+    return ENOMEM; //Sufficient virtual memory for the new process was not available.
+    }
+
+	*tf_child = *tf;
 
   /*Since the parent and child have the same file table: */
 
   for(int i = 0; i < OPEN_MAX; i++){
     if(curproc->file_table[i] != NULL){
       lock_acquire(curproc->file_table[i]->lock);//Lock the filetablei[i]
-      curproc->file_table[i]->ref_count++;   
+	  curproc->file_table[i]->ref_count ++;
       child_proc->file_table[i] = curproc->file_table[i];
       lock_release(curproc->file_table[i]->lock);//Release the lock at filetable[i]
     }
   }
 
-
-  err = thread_fork(curthread->t_name, child_proc,op_efp,(void *)tf_child, (unsigned long)0);
-  /*If thread_fork fails: destroy the process and free the trapframe memory*/
-  if(err){
-    proc_destroy(child_proc);
-    kfree(tf_child);
-    return err;
-  }
+    // Launch the child execution (first thread creation)
+    err = thread_fork("child",child_proc,(void*)enter_forked_process,
+                      tf_child,(unsigned long)child_proc->p_addrspace);
+    if(err){
+        return err;
+    }
 
 /*Set the return value as a child_PID */
   *child_pid = child_proc->proc_id;
