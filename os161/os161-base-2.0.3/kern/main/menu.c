b1280cae2b663865c23a484fd89143191c49ee15
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2023-02-02 16:03:25 +0100

    fix

--- os161/os161-base-2.0.3/kern/main/menu.c
+++ os161/os161-base-2.0.3/kern/main/menu.c
@@ -114,7 +114,6 @@ common_prog(int nargs, char **args)
 {
 	struct proc *proc;
 	int result;
-	//int status;
 
 	/* Create a process for the new program to run in. */
 	proc = proc_create_runprogram(args[0] /* name */);
