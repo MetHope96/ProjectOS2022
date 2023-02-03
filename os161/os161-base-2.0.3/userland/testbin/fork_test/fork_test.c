02355af3b1f20f9dfcebdb0c962137607164db34
Antonio-Colonna <72787385+Antonio-Colonna@users.noreply.github.com>
2023-02-03 09:52:15 +0100

    Update fork_test

--- os161/os161-base-2.0.3/userland/testbin/fork_test/fork_test.c
+++ os161/os161-base-2.0.3/userland/testbin/fork_test/fork_test.c
@@ -7,52 +7,40 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-int main ()
-{
+#define SUBARGC_MAX 64
+static char *subargv[SUBARGC_MAX];
 
-    pid_t pid1, pid2, pid3;
+int main(void){
+
+    static char default_prog[] = "/testbin/helloworld";
+    subargv[0] = default_prog;
+	
+    pid_t pid, pid1, pid2;
+    int status = 0;
+     int rval;
+
+    printf("Execution of fork\n");
+    
+    pid = fork();
+    if(pid < 0){ 
+        printf("Error fork: %s\n",strerror(errno)); 
+        return 1;
+    }else if(pid == 0){ // Child
+        pid1 = getpid();
+        printf("This is the child process with pid = %d\n and execution of execv",pid1);
+           
         
-        pid1 = fork();
-        
-        if(pid1 <0)
-        {
-            printf ("Error!\n");        
+        rval = execv(subargv[0], subargv);
+        if (rval == -1){
+            printf("Error execv: %s\n",strerror(errno));
+            return rval;
         }
-        else if (pid1 == 0 )
-        {
-           printf ("%d\n", getpid());
-           _exit(0);
-        }
-        else 
-        {
-            pid2 = fork();
-            if(pid2 <0)
-            {
-                printf ("Error!\n");        
-            }
-            else if (pid2 == 0 )
-            {
-            printf ("%d\n", getpid());
-             _exit(0);
-            }
-            else{
-                pid3 = fork();
-                if(pid3 <0)
-                {
-                printf ("Error!\n");        
-                }
-                else if (pid3 == 0 )
-                {
-                printf ("%d\n", getpid());
-                _exit(0);
-                }
-                else{
-                printf ("%d\n", getpid());
-                }
-
-            
-            }
-        }
-
-    return 0;
+    
+    }else{ // Parent
+        pid2 = getpid();
+        waitpid(pid, &status, 0);
+        printf("This the parent process with pid = %d that is waiting for its child with pid = %d\n",pid2,pid);
+        printf("The child process is exited with exit status %d\n", status);
+        return 0;
+    }
 }
